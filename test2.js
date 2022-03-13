c785a4f8abe9feb3e6ce891d61b694566a862efd
Endeile <danilavasiliev2412@gmail.com>
2022-03-13 20:07:37 +0300

    появление каркаса машины

--- test2.js
+++ test2.js
@@ -1,49 +1,47 @@
+//подготовка поля игры
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 ctx.imageSmoothingEnabled = false;
 canvas.width = 1880;
 canvas.hight = 1080;
 
-clicks = 2;
 x = 0;
 
-const keys = [];
+//
 
-const player = {
-    x:0,
-    y:0,
-    width:50,
-    hight:150,
-    frameX:0,
-    frameY:0,
-    speed: 9,
-    moving: false,
-};
-
-const playerSprite = new Image();
-
-playerSprite.src = "car.png"
+//загрузка спрайтов и изображений
+const car_1 = new Image();
+car_1.src = "car.png";
 
 const background = new Image();
-background.src = "garage.png"
+background.src = "garage.png";
 
 const but = new Image();
-but.src = "parts2.png"
+but.src = "parts2.png";
 
-const car = new Image();
-car.src = "da.png"
+const createCar = new Image();
+createCar.src = "da.png";
 
 const tools = new Image();
-tools.src = "parts.png"
+tools.src = "parts.png";
 
+//
+
+//состояние изоброжений на экране
 var show = false;
 
+var showCar = false;
+
+//отображение ирговых элементов на экране
 function animate()
 {ctx.imageSmoothingEnabled = false;
 
     ctx.drawImage(background,0,0,1880,200);
 
-    ctx.drawImage(car,192,0,200,30);
+    ctx.drawImage(createCar,192,0,200,30);
+
+    if(showCar) 
+       ctx.drawImage(car_1,390,-21,1100,160);
 
     ctx.drawImage(but,1670,0,200,30);
 
@@ -51,7 +49,11 @@ function animate()
      ctx.drawImage(tools,0,-4,1000,200);
 
     requestAnimationFrame(animate);
+    
 }
+//
+
+//функции и условия элементов игры
 
 function showToolsList(event) {
  
@@ -63,7 +65,7 @@ function showToolsList(event) {
 
   document.addEventListener("click", printMousePos);
   
-  
+  document.addEventListener("click", summonCar);
 
   //document.addEventListener("click", showToolsList);
 
@@ -72,9 +74,18 @@ function printMousePos(event) {
      console.log(event.clientX,event.clientY);
       
      if(event.clientX>1133&&event.clientY<151)
-     show = true;  
+     show = true;
+     else show = false 
   }  
-    
-  
 
+function summonCar(event) {
+     
+    if(event.clientX<188&&event.clientY<177)
+     showCar= true;
+    else showCar = false 
+ }  
+//    
+  
+//запуск цикла перерисовки
 animate();
+//
